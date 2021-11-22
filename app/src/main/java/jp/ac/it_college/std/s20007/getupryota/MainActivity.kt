1973c32e0ecb53f9129b6003b3788ddb84db62db
s20014 <s20014@std.it-college.ac.jp>
2021-11-22 12:10:32 +0900

    ryota

--- app/src/main/java/jp/ac/it_college/std/s20007/getupryota/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20007/getupryota/MainActivity.kt
@@ -2,10 +2,13 @@ package jp.ac.it_college.std.s20007.getupryota
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s20007.getupryota.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
