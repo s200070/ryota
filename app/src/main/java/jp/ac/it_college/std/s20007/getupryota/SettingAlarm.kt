2bc1d1210a76a56a9464db1b973a61c26e8a05a4
s20014 <s20014@std.it-college.ac.jp>
2021-12-06 12:07:37 +0900

    ryota

--- app/src/main/java/jp/ac/it_college/std/s20007/getupryota/SettingAlarm.kt
+++ app/src/main/java/jp/ac/it_college/std/s20007/getupryota/SettingAlarm.kt
@@ -2,10 +2,13 @@ package jp.ac.it_college.std.s20007.getupryota
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s20007.getupryota.databinding.ActivitySettingAlarmBinding
 
 class SettingAlarm : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingAlarmBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySettingAlarmBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting_alarm)
+        setContentView(binding.root)
     }
 }
