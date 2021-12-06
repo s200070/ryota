e74b02a40ae03822a7bc0149ce388091a4dba0fb
s20014 <s20014@std.it-college.ac.jp>
2021-12-06 12:12:21 +0900

    ryota

--- app/src/main/java/jp/ac/it_college/std/s20007/getupryota/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20007/getupryota/MainActivity.kt
@@ -2,13 +2,34 @@ package jp.ac.it_college.std.s20007.getupryota
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import jp.ac.it_college.std.s20007.getupryota.databinding.ActivityMainBinding
+import jp.ac.it_college.std.s20007.getupryota.list.AlarmListAdapter
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val alarms =
+            mutableListOf<alarm>(
+                alarm("00:00","ryouta","平日",true),
+                alarm("00:00","taiki","祝日",false),
+                alarm("00:00","taiga","月火水木",true),
+                alarm("00:00","eito","毎日",true),
+            )
+
+        val listAdapter = AlarmListAdapter(applicationContext)
+        listAdapter.alarms = alarms
+
+        val listView: ListView = findViewById(R.id.listview) as ListView
+        listView.adapter = listAdapter
+
+        binding.listview.setOnItemClickListener { parent, view, position, id ->
+
+        }
+
     }
 }
