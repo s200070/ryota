d3e8c5ade7df62636d2900a45849f3c25b0ad39c
s20014 <s20014@std.it-college.ac.jp>
2021-12-09 12:26:21 +0900

    view

--- app/src/main/java/jp/ac/it_college/std/s20007/getupryota/repeatWeek.kt
+++ app/src/main/java/jp/ac/it_college/std/s20007/getupryota/repeatWeek.kt
@@ -2,8 +2,10 @@ package jp.ac.it_college.std.s20007.getupryota
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s20007.getupryota.databinding.ActivityRepeatWeekBinding
 
 class repeatWeek : AppCompatActivity() {
+    private lateinit var binding: ActivityRepeatWeekBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repeat_week)
